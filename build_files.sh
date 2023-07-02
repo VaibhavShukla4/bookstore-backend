819cb822483e2410178fdd23cbfbc2ff3cd74fa6
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-07-02 19:04:14 +0530

    commit-12th

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
 echo "BUILD START"
 python3 -m pip install -r requirements.txt
-apt-get update
-apt-get install -y sqlite3 libsqlite3-dev
+python3 -m pip install pysqlite3
 python3 manage.py collectstatic --noinput --clear
 echo "BUILD END"
